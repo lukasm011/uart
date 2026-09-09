d03da7f36fead094b09f5ecaa5689262c04bf8b3
lukas <lukas06mueller@gmail.com>
2026-09-10 00:21:41 +0200

    Seperated combinational from sequential logic in WRITE pipeline.

--- src/axi/axi_write.vhd
+++ src/axi/axi_write.vhd
@@ -36,45 +36,120 @@ architecture synth of axi_write is
     ------ 0x08 <=> Control (WRITE ONLY)
     ------ 0x0C <=> Status (READ ONLY)
     ------//
-    signal data_reg                                                                                                                               : std_logic_vector(WIDTH - 1 downto 0);
-    signal addr_load, addr_resp                                                                                                                   : unsigned(1 downto 0);
-    signal nop_decode, nop_load, stall_load, stall_resp, en_decode, en_load, en_resp, rw_valid_load, rw_valid_resp : std_logic;
+    signal data_reg, data_reg_next                                                                                                : std_logic_vector(WIDTH - 1 downto 0);
+    signal addr_load, addr_resp, addr_load_next, addr_resp_next                                                    : unsigned(1 downto 0);
+    signal rw_resp_next : std_logic_vector(1 downto 0);
+    signal nop_decode, nop_load, stall_load, stall_resp, en_decode, en_load, en_resp, rw_valid_load, rw_valid_resp, w_ready_load : std_logic;
+    signal nop_decode_next, nop_load_next, rw_valid_load_next, aw_ready_next, w_ready_load_next, uart_write_next : std_logic;
+    signal uart_rst_next, rw_valid_resp_next, uart_sel_next : std_logic;
 begin
     --//
-    -- DECODE
-    --//
-    --//
-    -- The stall flags of LOAD and RESP must be driven combinationally to stop the pipeline from progressing on the next rising edge. 
+    -- DECODE CLOCKED
     --//
     decode_clocked : process(clk)
     begin
         if (rising_edge(clk)) then
-            if(not rst) then
-                aw_ready <= '1';
-                nop_decode <= '1';
-                addr_load <= (others => '0');
+            aw_ready <= aw_ready_next;
+            nop_decode <= nop_decode_next;
+            addr_load <= addr_load_next;
+        end if;
+    end process;
+    --//
+    -- DECODE COMBINATIONAL
+    --//
+    decode_comb : process(all)
+    begin
+        if (not rst) then
+            aw_ready_next <= '1';
+            nop_decode_next <= '1';
+            addr_load_next <= (others => '0');
+        else
+            --//
+            -- Default values
+            --//
+            aw_ready_next <= aw_ready;
+            nop_decode_next <= '1';
+            addr_load_next <= addr_load;
+            if(en_decode) then
+                if(aw_valid) then
+                    if(aw_ready) then
+                        --// Both valid and ready active, can transact
+                        addr_load_next <= unsigned(aw_addr(3 downto 2));
+                        nop_decode_next <= '0';
+                    else
+                        aw_ready_next <= '1';
+                    end if;
+                end if;
             else
+                aw_ready_next <= '0';
                 --//
-                -- Default values
+                -- Do not disable the next stage if current stage is disabled.
+                -- If the next stage is the one blocking, disabling it will cause an infinite loop. 
                 --//
-                aw_ready <= '1';
-                if (en_decode) then
-                    nop_decode <= '1';
-                    if (aw_valid) then
-                        --// Stage enabled and address valid
-                        if (aw_ready) then
-                            --// Both ready and valid active at the same time, can transact
-                            nop_decode  <= '0';
-                            aw_ready  <= '1';
-                            addr_load <= unsigned(aw_addr(3 downto 2));
-                        else
-                            --// Only valid active, assert ready
-                            aw_ready <= '1';
-                        end if;
+                nop_decode_next <= '0';
+            end if;
+        end if;
+    end process;
+    
+    --//
+    -- LOAD Combinational
+    --//
+    load_comb: process(all) begin
+        if(not rst) then
+            nop_load_next <= '1';
+            w_ready_load_next <= '0';
+            rw_valid_load_next <= '0';
+            uart_write_next <= '0';
+            addr_resp_next <= (others => '0');
+            stall_load <= '0';
+            data_reg_next <= (others => '0');
+        else
+            --//
+            -- Default values
+            --//
+            nop_load_next <= nop_decode;
+            rw_valid_load_next <= '0';
+            uart_write_next <= '0';
+            addr_resp_next <= addr_resp;
+            w_ready_load_next <= w_ready_load;
+            stall_load <= '0';
+            if(en_load) then
+                if(w_valid) then
+                    if(w_ready) then
+                        --// Both valid and ready active, can transact
+                        data_reg_next <= w_data(WIDTH - 1 downto 0);
+                        nop_load_next <= '0';
+                        rw_valid_load_next <= '1';
+                        addr_resp_next <= addr_load;
+                        case addr_load is
+                            when to_unsigned(1, 2) =>
+                                -- TX data in
+                                --//WRITE ONLY
+                                uart_write_next <= '1';
+                                rw_resp_next <= "10" when uart_full_tx else "00";
+                                --SLVERR when TX full, else OKAY
+                            when to_unsigned(2, 2) =>
+                                -- CONTROL
+                                --//WRITE ONLY
+                                rw_resp_next <= "00";
+                            when others =>
+                                -- Invalid address, DECERR
+                                rw_resp_next <= "11";
+                        end case;
+                    else
+                        --// Only valid active, assert ready
+                        w_ready_load_next <= '1';
+                        nop_load_next <= '1';
+                        stall_load <= '1';
                     end if;
                 else
-                    aw_ready <= '0';
+                    --// Data not yet valid, stall
+                    nop_load_next <= '1';
+                    stall_load <= '1';
                 end if;
+            else
+                --// Stage blocked, do not transact
+                w_ready_load_next <= '0';
             end if;
         end if;
     end process;
@@ -84,114 +159,57 @@ begin
     load : process(clk)
     begin
         if (rising_edge(clk)) then
-            if(not rst) then
-                report("Klipa");
-                nop_load <= '1';
-                w_ready <= '0';
-                rw_valid_load <= '0';
-                uart_write <= '0';
-                addr_resp <= (others => '0');
-                rw_resp <= "00";
-            else
-                --//
-                -- Default values
-                --//
-                nop_load      <= nop_decode;
-                w_ready  <= '1';
-                rw_valid_load <= '0';
-                uart_write    <= '0';
-                if (en_load) then
-                    if (w_valid) then
-                        report("Here");
-                        --// Stage enabled and data valid
-                        if (w_ready) then
-                            report("Here too");
-                            --// Both ready and valid active at the same time, can transact
-                            data_reg      <= w_data(WIDTH - 1 downto 0);
-                            rw_valid_load <= '1';
-                            addr_resp     <= addr_load;
-                            --// Set up response
-                            case addr_load is
-                                when to_unsigned(1, 2) =>
-                                    -- TX data in
-                                    --//WRITE ONLY
-                                    rw_resp    <= "10" when uart_full_tx else "00";
-                                    --//SLVERR when TX full, else OKAY
-                                    uart_write <= '1';
-                                when to_unsigned(2, 2) =>
-                                    -- Control
-                                    --//WRITE ONLY
-                                    rw_resp <= "00";
-                                    --//OKAY
-                                when others =>
-                                    -- Invalid address
-                                    rw_resp <= "11";
-                                    --//DECERR
-                            end case;
-                        else
-                            --// Only valid active, assert ready
-                            w_ready <= '1';
-                        end if;
-                    else
-                        --// Block next stage
-                        nop_load <= '1';
-                    end if;
-                else
-                    -- Disabled, cannot transact
-                    w_ready <= '0';
-                end if;
-            end if;
+            data_reg <= data_reg_next;
+            nop_load <= nop_load_next;
+            rw_valid_load <= rw_valid_load_next;
+            uart_write <= uart_write_next;
+            addr_resp <= addr_resp_next;
+            w_ready_load <= w_ready_load_next;
+            rw_resp <= rw_resp_next;
         end if;
     end process;
     --//
-    -- LOAD Combinational
+    -- RESP Combinational
     --//
-    load_comb : process(all)
-    begin
-        stall_load <= '0';
-        if(en_load) then
-            if(not w_valid or not w_ready) then
-                stall_load <= '1';
+    resp_comb : process(all) begin
+        if(not rst) then
+            uart_sel_next <= '0';
+            uart_rst_next <= '1';
+            rw_valid_resp_next <= '0';
+            stall_resp <= '0';
+        else
+            --//
+            -- Default values
+            --//
+            uart_sel_next <= uart_sel;
+            uart_rst_next <= '1';
+            stall_resp <= '0';
+            rw_valid_resp_next <= rw_valid_resp;
+            if(en_resp) then
+                if(rw_valid) then
+                    if(rw_ready = '1' and addr_resp = 2) then
+                        --TODO: Check whether data_reg can be overwritten in case of a RESP stall
+                        uart_rst_next <= not data_reg(0);
+                        uart_sel_next <= data_reg(1);
+                    end if;
+                else
+                    -- Set up valid data, stall for one cycle
+                    stall_resp <= '1';
+                    rw_valid_resp_next <= '1';
+                end if;
+            else
+                -- Not enabled, no response
+                rw_valid_resp_next <= '0';
             end if;
         end if;
     end process;
     --//
     -- RESP Clocked
     --//
-    resp : process(clk)
-    begin
-        if (rising_edge(clk)) then
-            if(not rst) then
-                uart_rst <= '1';
-                rw_valid_resp <= '0';
-                uart_sel <= '0';
-            else
-                --//
-                -- Default values
-                --//
-                if (en_resp) then
-                    uart_rst <= '1';
-                    if (rw_ready) then
-                        rw_valid_resp <= '0';
-                        if (addr_resp = 2) then
-                            -- Control
-                            uart_rst <= not data_reg(0);
-                            uart_sel <= data_reg(1);
-                        end if;
-                    end if;
-                end if;
-            end if;
-        end if;
-    end process;
-    --//
-    -- RESP Combinational
-    --//
-    resp_comb : process(all)
-    begin
-        stall_resp <= '0';
-        if (en_resp and not rw_ready) then
-            stall_resp <= '1';
-        end if;
+    resp_clocked : process(clk) begin
+        uart_sel <= uart_sel_next;
+        uart_rst <= uart_rst_next;
+        rw_valid_resp <= rw_valid_resp_next;
     end process;
     --//
     -- ENABLE SIGNALS
@@ -203,5 +221,6 @@ begin
     -- Misc.
     --//
     uart_d_in_ser <= data_reg;
+    w_ready <= w_ready_load and en_load;
     rw_valid      <= rw_valid_resp or rw_valid_load;
 end;
